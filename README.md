cd5d7f82653f608bc6a2ac60c710e1677ecf7757
imran-sheikh-bertelsmann <imran.sheikh@bertelsmann.de>
2026-01-19 14:22:37 +0800

    entryfile file update for naming convention

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 docker build -t itsmsb-worker-image .
 
 # This code will run in the 
-docker run -e BRANCH=main -e PATH_TO_MAIN=jsm-puller/main.py ghcr.io/imran-sheikh-bertelsmann/itsmsb-worker:latest
-docker run -e BRANCH=feature-a -e PATH_TO_MAIN=jsm-pusher/main.py ghcr.io/imran-sheikh-bertelsmann/itsmsb-worker:latest
+docker run -e branch=main -e path_to_main=jsm-puller/main.py ghcr.io/imran-sheikh-bertelsmann/itsmsb-worker:latest
+docker run -e branch=feature-a -e path_to_main=jsm-pusher/main.py ghcr.io/imran-sheikh-bertelsmann/itsmsb-worker:latest
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
 
-BASE_DIR="/itsmsb-code-repo"
+base_dir="/itsmsb-code-repo"
 
-BRANCH="${branch:?branch not set}"
+branch="${branch:?branch not set}"
 echo "Selected branch: $branch"
 
-PATH_TO_MAIN="${path_to_main:?path_to_main not set}"
+path_to_main="${path_to_main:?path_to_main not set}"
 echo "Selected path to main file: $path_to_main"
 
-MAIN_FILE="$BASE_DIR/$branch/$path_to_main"
-echo "Main file location: $MAIN_FILE"
+main_file="$base_dir/$branch/$path_to_main"
+echo "Main file location: $main_file"
 
-if [ ! -f "$MAIN_FILE" ]; then
+if [ ! -f "$main_file" ]; then
   echo "ERROR: File not found!"
   exit 1
 fi
 
-cd "$(dirname "$MAIN_FILE")"
+cd "$(dirname "$main_file")"
 
-python "$(basename "$MAIN_FILE")"
+python "$(basename "$main_file")"
